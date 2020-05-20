d991aa7708a10a387cbba90d8e146b71496d5832
Benjamin Halbert <bhalbert2@mail.greenriver.edu>
2020-05-20 17:20:19 -0400

    added survey page

---
+++ controllers/controller.php
@@ -0,0 +1,5 @@
+<?php
+
+function getChecks(){
+    return $checks = array("Yes", "Having Fun", "This is not too bad");
+}
--- index.php
+++ index.php
@@ -3,7 +3,10 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+session_start();
+
 require_once('vendor/autoload.php');
+require_once('controllers/controller.php');
 
 //Instantiate the F3 Base class
 $f3 = Base::instance();
@@ -12,6 +15,15 @@ $f3 = Base::instance();
 $f3->route('GET /', function() {
     $view = new Template();
     echo $view->render('views/home.html');
+});
+
+$f3->route('GET|POST /survey', function($f3) {
+    $checks = getChecks();
+
+    $f3->set('checks', $checks);
+    $view = new Template();
+    echo $view->render('views/survey.html');
+
 });
 
 //Run fat free
